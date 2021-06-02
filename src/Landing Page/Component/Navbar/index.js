6835cdaf4a95474ba39bbbe4b19171f42ba0c191
Yuba Neupane <neopaneyb2@gmail.com>
2021-06-02 00:28:49 -0400

    added sidebar menu

--- src/Landing Page/Component/Navbar/index.js
+++ src/Landing Page/Component/Navbar/index.js
@@ -12,13 +12,13 @@ import {
   NavBtnLink,
 } from './NavbarElements';
 
-export const Navbar = () => {
+export const Navbar = ({ toggle }) => {
   return (
     <>
       <Nav>
         <NavbarContainer>
           <NavLogo to="/">Super MANG</NavLogo>
-          <MobileIcon>
+          <MobileIcon onClick={toggle}>
             <FaBars />
           </MobileIcon>
           <NavMenu>
--- src/Landing Page/index.js
+++ src/Landing Page/index.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar } from './Component/Navbar';
+import { Sidebar } from './Component/Sidebar';
 
 function LandingPage() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div>
-      <Navbar />
+      <Sidebar isOpen={isOpen} toggle={toggle} />
+      <Navbar toggle={toggle} />
     </div>
   );
 }
