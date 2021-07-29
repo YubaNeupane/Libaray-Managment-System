dd98bc2cd843f9c57d90b3e9cd4532b5886f913f
charlie <charlie@charlies-MBP-2.fios-router.home>
2021-07-29 10:52:22 -0400

    fixed sizing

--- src/Dashboard/pages/Home/Carousel.js
+++ src/Dashboard/pages/Home/Carousel.js
@@ -1,12 +1,23 @@
 import React, { useState } from 'react';
 import Carousel from 'react-elastic-carousel';
 import Card from '../Search/Card';
+import { makeStyles } from '@material-ui/core/styles';
 
 import Grid from '@material-ui/core/Grid';
 
-const breakPoints = [{ width: 20, itemsToShow: 4 }];
+const breakPoints = [{ width: 100, itemsToShow: 3 }];
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+   
+  },
+
+}));
 
 export default function Slider() {
+
+  const classes = useStyles();
+
   const [books, setBooks] = useState(JSON.parse(localStorage.getItem('books')));
   const newBook = books.sort(function (a, b) {
     return new Date(b.lastUpdated) - new Date(a.lastUpdated);
@@ -15,8 +26,8 @@ export default function Slider() {
   return (
     <Carousel breakPoints={breakPoints}>
       {newBook.slice(0, 7).map((book) => (
-        <Grid key={book.id} item spacing={50}>
-          <Card book={book} display={true} />
+        <Grid key={book.id} item spacing={100} className={classes.container} >
+          <Card book={book} display={true} className={classes.container}/>
         </Grid>
       ))}
     </Carousel>
